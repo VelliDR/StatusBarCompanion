445dff25d207dab055605db79d9be138cb344276
VelliDR <veliozkul45@gmail.com>
2026-08-27 15:21:15 +0300

    chore: bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vellli.statusbarcompanion"
         minSdk = 34
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 
     buildTypes {
